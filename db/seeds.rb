958a57c4ed999132ebd3af838819e982d1d7fc71
NicolasHov <kola.hov@gmail.com>
2017-11-06 19:39:44 +0100

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.destroy_all
+Flight.destroy_all
+
+# Create Airoports
+airport_code_tab = ["ATL","BJS", "DXB", "LAX", "HND", "ORD", "LHR", "HKG", "PVG", "CDG", "DFW", "AMS", "FRA", "IST"]
+
+airport_code_tab.each do |airport_code_elem|
+  airport = Airport.create(airport_code: airport_code_elem)
+end
+
+# Create Flights
+all_airports = Airport.all
+index = all_airports.length - 1
+
+13.times do
+  departure_airport = all_airports[rand(0..index)].id
+  destination_airport = all_airports[rand(1..index)].id
+  next if departure_airport == destination_airport
+	  Flight.create(departure_date: rand(7.days).seconds.from_now,
+	  							duration: rand(500),
+	                departure_airport_id: departure_airport,
+	                arrival_airport_id: destination_airport)
+end
