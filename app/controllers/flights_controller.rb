e44adcab55c4928f28553bc49a303690d6756bdc
Yvan Sraka <yvan.sraka@gmail.com>
2017-11-07 17:47:59 +0100

    Fix error 'airport must exist'

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,19 +2,14 @@ class FlightsController < ApplicationController
 
   def index
     @flights = Flight.all
-    @airports = Airport.all
   end
 
   def new
     @flights = Flight.all
-    @airports = Airport.all
     @flight = Flight.new
   end
 
   def create
-    # TODO erreur 'airport must exist'
-    @airports = Airport.all
-
     @flights = Flight.all
     @flight = Flight.new(params[:flight])
     if @flight.save!
