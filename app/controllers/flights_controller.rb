c570ce0a451cd516179dd28ddd341b1706dfda87
Yvan Sraka <yvan.sraka@gmail.com>
2017-11-07 17:46:12 +0100

    Fix wrong indented file

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,26 +5,28 @@ class FlightsController < ApplicationController
     @airports = Airport.all
   end
 
-def new
-  @flights = Flight.all
-  @airports = Airport.all
-  @flight = Flight.new
-end
-
-def create
-  # TODO erreur 'airport must exist'
-  @airports = Airport.all
-
-  @flights = Flight.all
-  @flight = Flight.new(params[:flight])
-  if @flight.save!
-    redirect_to @flight
-  else
-    render 'new'
+  def new
+    @flights = Flight.all
+    @airports = Airport.all
+    @flight = Flight.new
   end
-end
-#pour montrer un vol en particulier .... faire la vue ...
+
+  def create
+    # TODO erreur 'airport must exist'
+    @airports = Airport.all
+
+    @flights = Flight.all
+    @flight = Flight.new(params[:flight])
+    if @flight.save!
+      redirect_to @flight
+    else
+      render 'new'
+    end
+  end
+
+  #pour montrer un vol en particulier .... faire la vue ...
   def show
      @flight = Flight.find(params[:id])
   end
+
 end
